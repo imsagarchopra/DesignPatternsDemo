0a78eeeddcbeb5c6b73182e7c78506e9e59eeb61
Sagar Chopra <schopra@firemate.com>
2023-08-11 12:23:53 +0530

    Singleton Pattern - With No Thread Safety

---
+++ SingletonPattern_NoThreadSafe/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace SingletonPattern_NoThreadSafe
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Call the GetInstance static method to get the Singleton Instance
+            Singleton fromTeachaer = Singleton.GetInstance();
+            fromTeachaer.PrintDetails("From Teacher");            
+
+            //Call the GetInstance static method to get the Singleton Instance
+            Singleton fromStudent = Singleton.GetInstance();
+            fromStudent.PrintDetails("From Student");
+
+            //Instantiating singleton from a Derived class. 
+            //This violates Singleton Pattern Pattern.
+
+            //Singleton.DerivedSingleton derivedObj = new Singleton.DerivedSingleton();
+            //derivedObj.PrintDetails("From Derived");
+            //Console.ReadLine();
+        }
+    }
+}

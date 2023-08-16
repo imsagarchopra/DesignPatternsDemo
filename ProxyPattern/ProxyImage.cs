3901c7a73c94396b8b945d18d4265d73f41f86a2
Sagar Chopra <schopra@firemate.com>
2023-08-16 11:31:28 +0530

    Proxy Pattern

---
+++ ProxyPattern/ProxyImage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyPattern
+{
+    // The Proxy has an interface identical to the RealSubject.
+    public class ProxyImage : IImage
+    {
+        private RealImage realImage = null;
+        private string Filename { get; set; }
+        public ProxyImage(string filename)
+        {
+            Filename = filename;
+        }
+        public void DisplayImage()
+        {
+            if (realImage == null)
+            {
+                realImage = new RealImage(Filename);
+            }
+            realImage.DisplayImage();
+        }
+    }
+}

070fb4c0366c5de3f3e45afb66890ef91c42f75a
Sagar Chopra <schopra@firemate.com>
2023-08-17 13:47:45 +0530

    Chain of Responsibility Pattern

---
+++ ChainOfResponsibilityPattern/HundredHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibilityPattern
+{
+    //Concrete Handler 4
+    //The following class implement the Handler abstract class and 
+    //Provide Implementation for DispatchNote abstract method
+    public class HundredHandler : Handler
+    {
+        public override void DispatchNote(long requestedAmount)
+        {
+            //First Check the Number of 100 Notes To Be Dispatched
+            long numberofNotesToBeDispatched = requestedAmount / 100;
+            if (numberofNotesToBeDispatched > 0)
+            {
+                if (numberofNotesToBeDispatched > 1)
+                {
+                    Console.WriteLine(numberofNotesToBeDispatched + " Hundred notes are dispatched by HundredHandler");
+                }
+                else
+                {
+                    Console.WriteLine(numberofNotesToBeDispatched + " Hundred note is dispatched by HundredHandler");
+                }
+            }
+            //No Need to Check the Next Handler
+        }
+    }
+}
---
+++ ChainOfResponsibilityPattern/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ChainOfResponsibilityPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ATM atm = new ATM();
+            Console.WriteLine("Requested Amount 4600");
+            atm.Withdraw(4600);
+            Console.WriteLine("\nRequested Amount 1900");
+            atm.Withdraw(1900);
+            Console.WriteLine("\nRequested Amount 600");
+            atm.Withdraw(600);
+            Console.WriteLine("\nRequested Amount 750");
+            atm.Withdraw(750);
+            Console.Read();
+        }
+    }
+}

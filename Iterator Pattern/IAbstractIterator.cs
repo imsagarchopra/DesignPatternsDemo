29bb3afdaa0dee792529efbab69b7b96aad5c424
Sagar Chopra <schopra@firemate.com>
2023-08-17 12:20:25 +0530

    Iterator Pattern

---
+++ Iterator Pattern/IAbstractIterator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator_Pattern
+{
+    // Iterator Interface
+    // This is going to be an interface defining the operations for accessing and traversing elements in a sequence.
+    public interface IAbstractIterator
+    {
+        Employee First();
+        Employee Next();
+        bool IsCompleted { get; }
+    }
+}
